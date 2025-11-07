f437ccc1a07a73a00f403d19191d4e21b73d2081
Kamal Singh Bisht <reachbisht7@gmail.com>
2025-11-06 20:54:42 -0600

    Initialize configuration module for Elasticsearch Agent

---
+++ config/__init__.py
@@ -0,0 +1,5 @@
+"""Configuration module for Elasticsearch Agent"""
+from .config import config, AppConfig, ElasticsearchConfig, OpenAIConfig
+
+__all__ = ['config', 'AppConfig', 'ElasticsearchConfig', 'OpenAIConfig']
+
